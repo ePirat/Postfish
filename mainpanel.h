d6b07217c69a8c261eae6da838137f17f9f9803f
Christopher Montgomery <monty@xiph.org>
2003-10-12 00:44:15 +0000

    catch up local copy with CVS
    git-svn-id: https://svn.xiph.org/trunk/postfish@5458 0101bb08-14d6-0310-b084-bc0e0c8e3800

---
+++ mainpanel.h
@@ -0,0 +1,1 @@
+extern void mainpanel_go(int argc,char *argv[]);
---
+++ readout.h
@@ -0,0 +1,45 @@
+#ifndef __READOUT_H__
+#define __READOUT_H__
+
+#include <sys/time.h>
+#include <time.h>
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtkcontainer.h>
+#include <gtk/gtkframe.h>
+#include <gtk/gtknotebook.h>
+#include <gtk/gtklabel.h>
+
+G_BEGIN_DECLS
+
+#define READOUT_TYPE            (readout_get_type ())
+#define READOUT(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), READOUT_TYPE, Readout))
+#define READOUT_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), READOUT_TYPE, ReadoutClass))
+#define IS_READOUT(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), READOUT_TYPE))
+#define IS_READOUT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), READOUT_TYPE))
+
+typedef struct _Readout       Readout;
+typedef struct _ReadoutClass  ReadoutClass;
+
+struct _Readout{
+
+  GtkFrame frame;
+  GtkWidget *label;
+  GtkWidget *sizelabel;
+};
+
+struct _ReadoutClass{
+
+  GtkFrameClass parent_class;
+  void (* readout) (Readout *m);
+
+};
+
+GType          readout_get_type        (void);
+GtkWidget*     readout_new             (char *markup);
+void	       readout_set             (Readout *m,char *markup);
+
+G_END_DECLS
+
+#endif
+
