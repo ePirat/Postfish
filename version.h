98e252b6e25e41af14da8fa68afe9fd4ff62cd94
Christopher Montgomery <monty@xiph.org>
2004-02-17 12:09:03 +0000

    minor tuning
    git-svn-id: https://svn.xiph.org/trunk/postfish@5832 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- version.h
+++ version.h
@@ -1,2 +1,2 @@
-#define VERSION "$Id: version.h,v 1.30 2004/02/17 11:52:18 xiphmont Exp $ "
-/* DO NOT EDIT: Automated versioning hack [Tue Feb 17 06:50:57 EST 2004] */
+#define VERSION "$Id: version.h,v 1.31 2004/02/17 12:09:03 xiphmont Exp $ "
+/* DO NOT EDIT: Automated versioning hack [Tue Feb 17 07:07:58 EST 2004] */
