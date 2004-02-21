003997de0c8ecbdf3324acf4ce155e98dd01869b
Christopher Montgomery <monty@xiph.org>
2004-02-21 01:50:55 +0000

    "God I don't even remember."
    [Now up for 39 hours....]
    
    Monty
    
    
    git-svn-id: https://svn.xiph.org/trunk/postfish@5854 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- version.h
+++ version.h
@@ -1,2 +1,2 @@
-#define VERSION "$Id: version.h,v 1.37 2004/02/20 06:43:32 xiphmont Exp $ "
-/* DO NOT EDIT: Automated versioning hack [Fri Feb 20 01:37:20 EST 2004] */
+#define VERSION "$Id: version.h,v 1.38 2004/02/21 01:50:55 xiphmont Exp $ "
+/* DO NOT EDIT: Automated versioning hack [Fri Feb 20 03:14:29 EST 2004] */
